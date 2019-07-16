d89d2f69c77c8ccb57fd1ed7477c2eaa8ce43857
Sebas <sebastianponce104@gmail.com>
2019-07-16 15:27:02 -0500

    Agrege el scrip en la parte final para poder llamar la base de datos del sub_type y puse una condicion que funciona para que solo traiga los de logstica en el primero

--- script.js
+++
@@ -1,9 +0,0 @@
-const logistica = document.getElementById("logistica")
-
-logistica.addEventListener("logistica")
-
-let option = document.createElement("option")
-
-option.text = "Sillas";
-option.value = 1
-
