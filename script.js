31b184763b1648851b63ca3dc95cf28d4b55bade
natinat24 <natys240431@hotmail.com>
2019-07-16 12:32:30 -0500

    cambios de script

---
+++ script.js
@@ -0,0 +1,8 @@
+const logistica = document.getElementById("logistica")
+
+logistica.addEventListener("logistica")
+
+let option = document.createElement("option")
+
+option.text = "";
+option.value = 1
